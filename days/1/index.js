ab403b2e4f63ca38a30369fe0da51377425d1eac
Luka Prebil Grintal <luka.p.grintal@tretton37.com>
2021-12-01 13:27:36 +0100

    workspace linting setup

--- days/1/index.js
+++ days/1/index.js
@@ -5,21 +5,20 @@ const input = inputFile.split("\n").map(Number);
 
 // Setup for both parts
 const countIncreases = (input) =>
-  input
-    .map((value, index) => {
-      const nextValue = input[index + 1];
-      return nextValue && value < nextValue;
-    })
-    .filter(Boolean).length;
-
+	input
+		.map((value, index) => {
+			const nextValue = input[index + 1];
+			return nextValue && value < nextValue;
+		})
+		.filter(Boolean).length;
 // Setup for part 2
 const windowOfThree = input.reduce((acc, value, index) => {
-  const nextValue = input[index + 1];
-  const nextNextValue = input[index + 2];
-  if (nextValue && nextNextValue) {
-    acc.push(value + nextValue + nextNextValue);
-  }
-  return acc;
+	const nextValue = input[index + 1];
+	const nextNextValue = input[index + 2];
+	if (nextValue && nextNextValue) {
+		acc.push(value + nextValue + nextNextValue);
+	}
+	return acc;
 }, []);
 
 // Part 1
@@ -29,5 +28,5 @@ const increases = countIncreases(input);
 const windowedIncreases = countIncreases(windowOfThree);
 
 console.log(
-  `Number of increases: ${increases}. Number of increases with a window size of 3: ${windowedIncreases}`
+	`Number of increases: ${increases}. Number of increases with a window size of 3: ${windowedIncreases}`
 );
